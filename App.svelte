71fff85036ee1565d1d56a79f07d94f2b2dff794
José Calderón-Celis <josecalderoncelis@gmail.com>
2021-10-30 17:04:07 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Title from "./Title.svelte";
+	import Form from "./Form.svelte";
+</script>
+
+<main>
+	<Title>
+    Películas
+	</Title>
+	<Form />
+</main>
---
+++ Form.svelte
@@ -0,0 +1,31 @@
+<script>
+	const movies = [
+	  {
+	    name: "Avengers",
+	    quantity: 0,
+	    available: 3
+	  },
+	  {
+	    name: "Terminator",
+	    quantity: 0,
+	    available: 5
+	  }
+	];
+</script>
+
+{#each movies as movie}
+	<form>
+		<p>¿Cuántos boletos para {movie.name}?</p>
+
+      <div>
+        <button type="button" on:click={() => movie.quantity--}
+        >-</button>
+
+        <span> {movie.quantity} </span>
+
+        <button
+					type="button" on:click={() => movie.quantity++}
+        >+</button>
+      </div>
+	</form>
+{/each}
